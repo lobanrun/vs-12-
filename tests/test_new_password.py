d9e2a6cd05025978501712b85bbd8c140b8f8901
s1koi <shinkaruk140@mail.ru>
2026-02-22 16:03:37 +0600

    +проверка на длину + проверка на повторяемость

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -7,7 +7,16 @@ def test_password_characters():
     password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
     for char in password:
         assert char in valid_characters
-
+def test_password_not_s():
+    """Тест, что при генерации используются только допустимые символы"""
+    password1 = generate_password(10)  # Генерируем длинный пароль для более надежной проверки
+    password2 = generate_password(10)
+    assert password1 != password2
+def test_password_len():
+    """Тест, что при генерации используются только допустимые символы"""
+    password1 = generate_password(10)  # Генерируем длинный пароль для более надежной проверки
+    leng = len(password1)
+    assert leng == 10
 """
 Допиши еще один тест из предложенных. Или придумай свой.
 Если сможешь написать больше, то будет круто!
