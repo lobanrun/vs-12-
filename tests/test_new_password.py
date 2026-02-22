aa0a71f8515e957418b5a8e9201b44689daa12a3
wth <ancoussana95@gmail.com>
2026-02-22 15:14:59 +0500

    тесты

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,6 +8,23 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
+
+def test_passwords12_bySanya():
+    password1 = generate_password(10)
+    password2 = generate_password(10)
+    assert password1 != password2 
+
+def testpasswordlenght_sanya():
+    expected_lenght = 12
+    password = generate_password(expected_lenght)
+    assert len(password) == expected_lenght
+
+def testspecialsymbol():
+    special = string.punctuation
+    password = generate_password(10)
+    assert any(c in special for c in password)
+
+
 """
 Допиши еще один тест из предложенных. Или придумай свой.
 Если сможешь написать больше, то будет круто!
