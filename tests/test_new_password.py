dc8a178217ddbbbe5a43889cf997884c124e3cb2
Христофор <hristoforsitnikov2@gmail.com>
2026-02-22 13:34:55 +0300

    тест от христофора

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -4,14 +4,13 @@ from password.new_password import generate_password
 def test_password_characters():
     """Тест, что при генерации используются только допустимые символы"""
     valid_characters = string.ascii_letters + string.digits + string.punctuation
-    password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
+    password = generate_password(100) 
     for char in password:
         assert char in valid_characters
 
-"""
-Допиши еще один тест из предложенных. Или придумай свой.
-Если сможешь написать больше, то будет круто!
+def test_default(): 
+    """ Тест проверяет значение по умолчанию """
+    assert len(generate_password()) == 12
 
-Тест, что длина пароля соответствует заданной
-Тест, что два сгенерированных подряд пароля различаются
-"""
+def test_hristofor():
+    pass 
