61d81dea86f937e1f46b0a83c47f559de306b036
NeuroGun <hristoforsitnikov2@gmail.com>
2026-02-22 13:29:01 +0300

    test_new_password.py

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,10 +8,6 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
-"""
-Допиши еще один тест из предложенных. Или придумай свой.
-Если сможешь написать больше, то будет круто!
-
-Тест, что длина пароля соответствует заданной
-Тест, что два сгенерированных подряд пароля различаются
-"""
+def test_default(): #Ilja Prusakovs
+    """ Тест проверяет значение по умолчанию """
+    assert len(generate_password()) == 12
